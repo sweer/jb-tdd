55799c0b90ebca72c45579187276d2f948dbe602
sweer <svires@gmail.com>
2011-10-28 19:51:17 +0300

    AreaTest done, MainTest part 2 in progress: factored out the view, now it's model's turn

--- store4/src/test/AreaTest.java
+++ store4/src/test/AreaTest.java
@@ -7,21 +7,27 @@ package test;
  *               0  1  2  3  4  5  6  7  8
  * done Area of [3, 2, 7, 3, 5, 9, 1, 3, 2] item #3 of size 5 = [2, 7, 3, 5, 9] 
  * 
- * Area of item #5 of size 4 = [5, 9, 1, 3]
+ * done Area of item #5 of size 4 (even) = [5, 9, 1, 3]
  * 
- * Area of item #0 of size 5 = [3, 2, 7, 3, 5] 
+ * Area of item #0 of size 5 (odd, at left edge) = [3, 2, 7, 3, 5] 
  * 
- * Area of item #8 of size 5 = [5, 9, 1, 3, 2]
+ * Area of item #8 of size 5 (odd, at the right edge) = [5, 9, 1, 3, 2]
  * 
  * #1 & #7 ? 
  */
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AreaTest {
 	
+	private int data[];
+	private Areator areator;
+
+
+
 	static class Areator {
 
 		private final int[] data;
@@ -33,21 +39,33 @@ public class AreaTest {
 		public int[] getAreaOfPointWithSize(int index, int size) {
 			int[] result = new int[size]; 
 			for (int i = 0; i < size; i++) { 
-				result[i] = data[index - size/2 + i]; 
+				result[i] = data[index - (size - 1) / 2 + i]; 
 			}
 			return result; 
 		} 
 		
 	}
+
+	@Before
+	public final void setup() { 
+		data = new int[] {3, 2, 7, 3, 5, 9, 1, 3, 2};
+		areator = new Areator(data);
+	}
 	
 	@Test
-	public final void testAreaInTheMiddle() {
-		int data[] = {3, 2, 7, 3, 5, 9, 1, 3, 2};
-		Areator areator = new Areator(data);
+	public final void testEvenAreaInTheMiddle() {
+		int[] area = areator.getAreaOfPointWithSize(5, 4); 
 		
+		assertArrayEquals(new int[] {5, 9, 1, 3}, area); 
+	}
+
+
+	
+	@Test
+	public final void testOddAreaInTheMiddle() {
 		int[] area = areator.getAreaOfPointWithSize(3, 5); 
 		
-		assertArrayEquals(area, new int[] {2, 7, 3, 5, 9}); 
+		assertArrayEquals(new int[] {2, 7, 3, 5, 9}, area); 
 	}
 
 }
--- store4/src/test/MainTest.java
+++ store4/src/test/MainTest.java
@@ -9,39 +9,67 @@ import org.junit.Before;
 import org.junit.Test;
 
 /*
- * Tests: 
- * 
- * There is a POS, it has a handler for the barcode scanned event.
- * 
- * done Scanning barcode 11111 the display displays price 21.13
- * 
- * done Scanning barcode 33333 the display displays price 33.17
- * 
- * done Scanning unknown barcode 22222 it displays "No price for 22222" 
- * 
- * done Scanning empty barcode it displays "Empty barcode scanned"
- * 
- * At this point it's evident we've view, controller and model all in one Pos class. 
- * String variable called "price" stinks. 
- * 
  * Req-ts: 
+ * There is a POS, it has a handler for the barcode scanned event.
  * Some products have GST 5% and some additional PST 6% on top of it. 
  * Price display shall include G or GP indicator. 
  * Pressing "total" shall display cost = price + all applicable taxes. 
  * 
  * Tests: 
  * 
+ * done Scanning unknown barcode 22222 it displays "No price for 22222" 
  * 
+ * done Scanning empty barcode it displays "Empty barcode scanned"
+ *
+ * done Scanning 11111 displays 21.13, 
+ * done clicking Total then displays 21.13 
+ * 
+ * done Scanning 33333 displays 33.17 G, 
+ * clicking Total then displays 34.83
+ * 
+ * Scanning 44444 displays 444.11 GP, clicking Total then displays 494.29  
+ * 
+ * Clicking Total after total price displayed does nothing. 
+ * 
+ * Clicking Total before scanning anything displays "Scan something first".
+ *
+ * 
+ * Notes: 
+ * 
+ * We've controller and model all in one Pos class. 
+ * Double math arithmetics is dangerous. 
  * 
  */
 
 public class MainTest {
 
+	enum TaxType { 
+		NO_TAX, GST, GST_AND_PST;  
+	}
+	
+	static final class Price { 
+		private final double price; 
+		private final TaxType taxType;
+
+		
+		public double getPrice() {
+			return price;
+		}
+		public TaxType getTaxType() {
+			return taxType;
+		}
+		public Price(double price, TaxType taxType) {
+			super();
+			this.price = price;
+			this.taxType = taxType;
+		}   
+	}
+	
 	static class Pos {
 		private final Display display;
-		private final Map<String, String> barcodeToPriceMap;
+		private final Map<String, Price> barcodeToPriceMap;
 		
-		public Pos(Display display, Map<String, String> barcodeToPriceMap) {
+		public Pos(Display display, Map<String, Price> barcodeToPriceMap) {
 			super();
 			this.display = display;
 			this.barcodeToPriceMap = barcodeToPriceMap;
@@ -49,42 +77,66 @@ public class MainTest {
 
 		void onBarcodeScanned(final String barcode) {
 			if (barcode.equals("")) { 
-				display.display("Empty barcode scanned"); 
+				display.displayEmptyBarcodeScanned(); 
 				return;
 			}
 			
-			String price = barcodeToPriceMap.get(barcode);
-			if (price != null) 
-				display.display(price);
-			else 
-				display.display("No price for " + barcode); 
+			Price price = barcodeToPriceMap.get(barcode);
+			if (price != null) {  
+				display.displayPrice(price.getPrice(), price.getTaxType());  
+			} else {  
+				display.displayBarcodeNotFound(barcode); 
+			}
+		}
+
+		public void onTotal() {
+			// TODO Auto-generated method stub
+			
 		}
 	}
 	
 	static class Display {
 		private String lastDisplayedString;
-		
-		void display(final String s) { 
-			lastDisplayedString = s; 
+		private final Map<TaxType, String> taxLabel = new HashMap<TaxType, String>() {{
+			put(TaxType.NO_TAX, ""); 
+			put(TaxType.GST, " G"); 
+		}}; 
+		public void displayBarcodeNotFound(String barcode) {
+			sendToDisplay("No price for " + barcode);
 		}
 
+		public void displayPrice(double price, TaxType taxType) {
+			sendToDisplay(Double.toString(price) + taxLabel.get(taxType));
+		}
+
+
+		public void displayEmptyBarcodeScanned() {
+			sendToDisplay("Empty barcode scanned"); 
+		}
+
+		void sendToDisplay(String s) { 
+			lastDisplayedString = s; 
+		}
+		
 		public String getLastDisplayedString() {
 			return lastDisplayedString; 
 		}
 	}
 
+	
+	
 	private Display display; 
 	private Pos pos; 
-	private Map<String, String> barcodeToPriceMap;
+	private Map<String, Price> barcodeToPriceMap;
 	
 	@SuppressWarnings("serial")
 	@Before 
 	public void testSetup() { 
 		display = new Display();  
 
-		barcodeToPriceMap = new HashMap<String, String>() { {
-			put("11111", "21.13"); 
-			put("irrelevant_data", "irrelevant_data"); 
+		barcodeToPriceMap = new HashMap<String, Price>() { {
+			put("11111", new Price(21.13, TaxType.NO_TAX)); 
+			put("33333", new Price(33.17, TaxType.GST)); 
 		};
 		};
 		
@@ -97,9 +149,43 @@ public class MainTest {
 		
 		pos.onBarcodeScanned(barcode); 
 		
-		assertEquals(barcodeToPriceMap.get(barcode), display.getLastDisplayedString()); 
+		assertEquals(Double.toString(barcodeToPriceMap.get(barcode).getPrice()), display.getLastDisplayedString()); 
 	}
 
+	@Test
+	public void testScanKnownBarcode2() {
+		final String barcode = "33333"; 
+		
+		pos.onBarcodeScanned(barcode); 
+		
+		assertEquals(Double.toString(barcodeToPriceMap.get(barcode).getPrice()) + " G", display.getLastDisplayedString()); 
+	}
+
+//	@Test
+//	public void testTotalKnownBarcode2() {
+//		final String barcode = "33333"; 
+//		
+//		pos.onBarcodeScanned(barcode);
+//		
+//		pos.onTotal();
+//		
+//		assertEquals(String.format("%.2f", barcodeToPriceMap.get(barcode).getPrice()*1.05), display.getLastDisplayedString()); 
+//	}
+	
+	
+	
+	@Test
+	public void testTotalKnownBarcode() {
+		final String barcode = "11111"; 
+		
+		pos.onBarcodeScanned(barcode);
+		
+		pos.onTotal();
+		
+		assertEquals(Double.toString(barcodeToPriceMap.get(barcode).getPrice()), display.getLastDisplayedString()); 
+	}
+	
+	
 	
 
 	@Test
